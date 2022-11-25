86d2a234ae226affd28a6cd8b21cc92bb24abe63
Lima <suwaloff@gmail.com>
2022-11-25 11:40:35 +0300

    Добавьте файлы проекта.

---
+++ SearchAlgorithms.cs
@@ -0,0 +1,46 @@
+﻿namespace Algorithms
+{
+    public static class SearchAlgorithms
+    {
+        public static int Linear(int key, int[] Array)
+        {
+
+            if (Array is null)
+            {
+                return -2;
+            }
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] == key) return i;
+
+            }
+            return -1;
+        }
+        public static int BinaryFind(int key, int[] myArray)
+        {
+            int first = 0;
+            int last = myArray.Length;
+            int mid = myArray.Length / 2;
+
+            if (myArray is null)
+            {
+                return -2;
+            }
+
+            while (first <= last)
+            {
+                mid = (last + first) / 2;
+
+                if (myArray[mid] > key) last = mid - 1;
+
+                else if (myArray[mid] < key) first = mid + 1;
+
+                else break;
+            }
+
+            if (myArray[mid] == key) return mid;
+
+            return -1;
+        }
+    }
+}
---
+++ SortingAlgorithms.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms
+{
+
+    public static class SortingAlgorithms
+    {
+        public static void BubbleSortDW(int[] myArray)
+        {
+            int arrayLength = myArray.Length;
+            bool isNeedSorting = true;
+            int transpositionIndexArray;
+
+            do
+            {
+                transpositionIndexArray = 0;
+
+                for (int i = 0; i < arrayLength - 1; i++)
+                {
+                    if (myArray[i] > myArray[i + 1])
+
+                        (myArray[i], myArray[i + 1]) = (myArray[i + 1], myArray[i]);
+                    transpositionIndexArray++;
+                }
+                arrayLength--;
+
+                if (transpositionIndexArray == 0) isNeedSorting = false;
+
+            } while (isNeedSorting);
+        }
+    }
+}
