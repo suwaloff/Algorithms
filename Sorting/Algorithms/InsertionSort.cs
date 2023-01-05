923c7133ada87a61207a999c886b25ae57eb28fb
Lima <suwaloff@gmail.com>
2023-01-05 12:44:07 +0300

    up

---
+++ Sorting/Algorithms/InsertionSort.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms.Sorting.Algorithms
+{
+    public class InsertionSort : IStrategy
+    {
+        public int[] array { get; }
+
+        public InsertionSort(int[] array) => this.array = array;
+
+        public void Sort()
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int minIndex = i;
+
+                for (int nextIndex = i + 1; nextIndex < array.Length; nextIndex++)
+                {
+                    if (array[nextIndex] < array[minIndex])
+                    {
+                        minIndex = nextIndex;
+                    }
+                }
+
+                Helping.Swap(ref array[i], ref array[minIndex]);
+            }
+
+        }
+
+    }
+}
---
+++ UnitTest/SortingAlgorithmTest/InsertionSortTest.cs
@@ -0,0 +1,25 @@
+﻿using Algorithms.Search;
+using Algorithms.Sorting.Algorithms;
+using Algorithms.Sorting;
+using Xunit;
+
+namespace Algorithms.UnitTest.SortingAlgorithmTest
+{
+    public static class InsertionSortTest
+    {
+        [Fact]
+        public static void Insertion_Array_Test()
+        {
+            // Arrange
+            int[] array =     { 16, 1, 9, 17, 11, 15, 13, 14, 12, 7, 10 };
+            int[] sortArray = { 1, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+
+            // Act
+            Sorting.Sorting sorting1 = new Sorting.Sorting(new InsertionSort(array));
+            sorting1.Sort();
+
+            // Assert
+            Assert.Equal(sortArray, array);
+        }
+    }
+}
